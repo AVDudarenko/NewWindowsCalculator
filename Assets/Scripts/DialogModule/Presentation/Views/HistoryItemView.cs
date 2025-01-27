7a1bce1b5c94303936ecaffba5cae2fdbbe34476
Aliaksandr <dyvedd@gmail.com>
2025-01-27 13:12:03 +0300

    add comments

--- Assets/Scripts/DialogModule/Presentation/Views/HistoryItemView.cs
+++ Assets/Scripts/DialogModule/Presentation/Views/HistoryItemView.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 /*
 * Represents a single item in the calculation history UI.
 * Responsible for displaying a record of a calculation.
-*/ </summary>
+*/
 public class HistoryItemView : MonoBehaviour
 {
     [SerializeField] private TMP_Text _textField;
